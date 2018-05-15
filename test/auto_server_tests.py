7afcfea88beb589b94fdafa99a7af5f8c50c9b53
jonathanvdc <jonathan.vdc@outlook.com>
2018-05-15 12:09:47 +0200

    Use mock content tracker to run all manual unit tests during the auto-testing process

--- test/auto_server_tests.py
+++ test/auto_server_tests.py
@@ -4,6 +4,7 @@
 import unittest
 import requests
 import sys
+import common_tests
 from libclient import log, start_server, request_token, is_authenticated
 
 domain = 'http://localhost:8080'
@@ -16,9 +17,20 @@ class TestAuthentication(unittest.TestCase):
         assert not is_authenticated(domain, token)
 
 
+def sign_in_user(domain):
+    """Signs in a test user. Returns the user's token."""
+    login = 'test-user'
+    token = request_token(domain)
+    login_response = requests.get('%s/auth/auth/%s/%s' % (domain, token, login))
+    login_response.raise_for_status()
+    return token
+
+
 if __name__ == '__main__':
     server = start_server(domain, '--mock-content-tracker')
     try:
+        token = sign_in_user(domain)
+        common_tests.import_unit_tests(domain, token, globals())
         unittest.main()
     finally:
         server.kill()
